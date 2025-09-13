089afecc3dc7a92e33c2026c30189f7aabf18a6f
Jiaduobao26 <mitsuitoto@gmail.com>
2025-09-12 23:58:41 -0700

    chore(assets): add index.js to centralize image exports

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as friendsIcon } from './friends.png';
+export { default as budgetIcon } from './budget.png';
+export { default as travelIcon } from './travel.png';
+export { default as calendarIcon } from './wall-calendar.png';
+export { default as preferenceIcon } from './feedback.png';
+export { default as dishIcon } from './dish.png';
